564168c72aae8780e4181afc67c4c2b55b2b147a
xaviGH <fgargallo@gmail.com>
2019-05-06 21:35:32 +0200

    onmouseOver <span id="star1" class="far fa-star" onmouseover="mark(0)"> </span>

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 //Rating exercise
 //@Xavi Gargallo
+// add at the end of the HTML to define global variables depending of DOM elements
+// because the file html needs to load first.
 
   var star1 = document.querySelector("#star1");
   var star2 = document.querySelector("#star2");
@@ -7,18 +9,20 @@
   var star4 = document.querySelector("#star4");
   var star5 = document.querySelector("#star5");
 
+   // star1.addEventListener("mouseover", mark, true );
+
 // keep variable DOM elements inside an array
  var starsArray = [star1, star2, star3, star4, star5];
 
 // check stars
 function mark(n){
-reset();
+  reset();
       for(var i=0; i<=n; i++) {
         starsArray[i].classList.toggle("marked",true);
         // console.log(starsArray[i]);
       }
-}
 
+}
 function reset(){
   for(var i=0; i<=4; i++) {
     starsArray[i].classList.toggle("marked", false);
