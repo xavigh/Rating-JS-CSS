67b33e4f25f8314d6cc276dcba222eb869e91505
xaviGH <fgargallo@gmail.com>
2019-05-06 18:47:07 +0200

    star rating working with onclick event and reset function.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,5 @@
-
-
-
-
-
-// check stars
-function mark(n){
+//Rating exercise
+//@Xavi Gargallo
 
   var star1 = document.querySelector("#star1");
   var star2 = document.querySelector("#star2");
@@ -12,16 +7,21 @@ function mark(n){
   var star4 = document.querySelector("#star4");
   var star5 = document.querySelector("#star5");
 
-console.log("global variable = " + star1);
-
 // keep variable DOM elements inside an array
-var starsArray = [star1, star2, star3, star4, star5];
+ var starsArray = [star1, star2, star3, star4, star5];
 
-
+// check stars
+function mark(n){
+reset();
       for(var i=0; i<=n; i++) {
-
-        starsArray[i].classList.add("marked");
-        console.log(starsArray[i]);
+        starsArray[i].classList.toggle("marked",true);
+        // console.log(starsArray[i]);
       }
+}
 
+function reset(){
+  for(var i=0; i<=4; i++) {
+    starsArray[i].classList.toggle("marked", false);
+    console.log(starsArray[i]);
+  }
 }
