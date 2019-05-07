a7519fceda7dc818f4ed857dce5ba80c1afa9d7c
xavigk <fgargallo@gmail.com>
2019-05-07 13:42:55 +0200

    rating update
    It needs to control with CSS now, when click on stars.

--- js/main.js
+++ js/main.js
@@ -4,22 +4,19 @@
 // because the file html needs to load first.
 
   //get the element + i then use to add css through classList.add()
-
-   // star1.addEventListener("mouseover", mark, true );
-
-
+    starElements.addEventListener("click", clickOnStar, true );
 
 // check stars
 //we loop through DOM elem and change their css Class
 // to display differently
 function mark(n){
   reset();
-
       for(var i=1; i<=n; i++) {
         let elemId = document.querySelector("#star"+i);
         elemId.classList.toggle("marked",true);
       }
 }
+
 function reset(){
   for(var i=1; i<=5; i++) {
       let elemId = document.querySelector("#star"+i);
@@ -27,15 +24,20 @@ function reset(){
         console.log("elemId= "+ elemId);
   }
 }
-window.addEventListener("click",resetScreen);
+
+// events listeners to catch click anyhere in the screen but
+// in the DOM element Id's that target our stars.
+window.addEventListener("click",reset);
 starElements.addEventListener("click",stopReset);
 
-// event listener to catch click anyhere in the screen but
-// in the DOM element Id's that target our stars.
-function resetScreen(){
-  reset();
-
-}
 function stopReset(e){
     e.stopPropagation();
+}
+
+function clickOnStar(e){
+  window.removeEventListener("click",reset);
+  starElements.removeEventListener("click",stopReset);
+
+  var a = e.currentTarget.innerHTML;
+  console.log("currentTarget = "+a );
 }
