7bf9424e9ab2cd2ea1d513d6c3228150a924e84a
xavigk <fgargallo@gmail.com>
2019-05-07 10:32:10 +0200

    No need array

--- js/main.js
+++ js/main.js
@@ -3,29 +3,38 @@
 // add at the end of the HTML to define global variables depending of DOM elements
 // because the file html needs to load first.
 
-  var star1 = document.querySelector("#star1");
-  var star2 = document.querySelector("#star2");
-  var star3 = document.querySelector("#star3");
-  var star4 = document.querySelector("#star4");
-  var star5 = document.querySelector("#star5");
+  //get the element + i then use to add css through classList.add()
 
    // star1.addEventListener("mouseover", mark, true );
 
-// keep variable DOM elements inside an array
- var starsArray = [star1, star2, star3, star4, star5];
+
 
 // check stars
+//we loop through DOM elem and change their css Class
+// to display differently
 function mark(n){
   reset();
-      for(var i=0; i<=n; i++) {
-        starsArray[i].classList.toggle("marked",true);
-        // console.log(starsArray[i]);
-      }
 
+      for(var i=1; i<=n; i++) {
+        let elemId = document.querySelector("#star"+i);
+        elemId.classList.toggle("marked",true);
+      }
 }
 function reset(){
-  for(var i=0; i<=4; i++) {
-    starsArray[i].classList.toggle("marked", false);
-    console.log(starsArray[i]);
+  for(var i=1; i<=5; i++) {
+      let elemId = document.querySelector("#star"+i);
+      elemId.classList.toggle("marked", false);
+        console.log("starId= "+elemId.innerHtml);
   }
+}
+
+// event listener to catch click anyhere in the screen but
+// in the DOM element Id's that target our stars.
+
+function resetScreen(e){
+
+   document.addEventListener("onclick", function(e){
+     
+   })
+
 }
