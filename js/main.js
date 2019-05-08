ac27d092b544a6d8c22a151b08a11bc923be459a
xavigk <fgargallo@gmail.com>
2019-05-08 12:02:17 +0200

    stars rating working
    Stars rating working, added

--- js/main.js
+++ js/main.js
@@ -1,43 +1,48 @@
-//Rating exercise
-//@Xavi Gargallo
+//Rating star exercise, Xavi Gargallo Jané
 
 
-// check stars
-//we loop through DOM elem and change their css Class
-// to display differently
+//we loop through DOM elem and change the css Class to display differently
+// events listeners to catch click anyhere in the screen but
+// in the DOM element Id's that target our stars.
+window.addEventListener("click",reset);
+window.addEventListener("click",resetStarsClicked);
+starElements.addEventListener("click",stopReset);
+
+//Add marked class
 function mark(n){
-
-  reset();
+      reset();
       for(var i=1; i<=n; i++) {
         let elemId = document.querySelector("#star"+i);
         elemId.classList.toggle("marked",true);
       }
 }
-
+//Reset the stars
 function reset(){
-  for(var i=1; i<=5; i++) {
-      let elemId = document.querySelector("#star"+i);
-      elemId.classList.toggle("marked", false);
-        // console.log("elemId= "+ elemId);
-  }
+      for(var i=1; i<=5; i++) {
+          let elemId = document.querySelector("#star"+i);
+          elemId.classList.toggle("marked", false);
+         // console.log("elemId= "+ elemId);
+      }
 }
+//reset the stars when clicked on them.
+function resetStarsClicked() {
 
-// events listeners to catch click anyhere in the screen but
-// in the DOM element Id's that target our stars.
-window.addEventListener("click",reset);
-starElements.addEventListener("click",stopReset);
-
-
+      for(var i=1; i<=5; i++) {
+          let elemId = document.querySelector("#star"+i);
+          elemId.classList.toggle("starClick", false);
+            // console.log("elemId= "+ elemId);
+      }
+}
+// if user clicks on star then stop reset.
 function stopReset(e){
     e.stopPropagation();//this stops reset()
 }
-
+// if user click on star then add css class starClick.
 function clickOnStar(n){
-  // window.removeEventListener("click",reset);
-  // starElements.removeEventListener("click",stopReset);
-  for(var i=1; i<=n; i++) {
-    let elemId = document.querySelector("#star"+i);
-    elemId.classList.toggle("starClick",true);
-  }
+      resetStarsClicked();
 
+      for(var i=1; i<=n; i++) {
+        let elemId = document.querySelector("#star"+i);
+        elemId.classList.toggle("starClick",true);
+      }
 }
